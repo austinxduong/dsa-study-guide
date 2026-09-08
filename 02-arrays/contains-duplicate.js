bcfb17dd5c002b2f8e00ac2c24a40182609d3b4d
austinxduong <austinxduong@gmail.com>
2026-09-08 00:34:17 -0700

    contains-duplicate algo

--- 02-arrays/contains-duplicate.js
+++ 02-arrays/contains-duplicate.js
@@ -1,6 +1,19 @@
 console.log("JavaScript is working!");
 console.log("DSA environment is ready.");
 
-function containsDuplicate(nums) {
-// your solution goes here
-}
+
+function hasDuplicate(nums) {
+        for (let i =0; i < nums.length; i++) {
+            for (let j = i + 1; j < nums.length; j++) {
+                if (nums[i] === nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false
+    }
+
+    // return false only executes once i has exhausted every value up to nums.length i.e., the outer loop is completely done, not just one pass through it.
+    // i stays parked, and j does a sweep through the nums array comparing it to the parked i if it matches - return true 
+    // O(n²) time - because for every item, where are doing something with every other item, and its a nested loop
+    // O(1) space - because where only holding i and j in memory once. regardless of input size
