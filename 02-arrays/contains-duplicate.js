b410a30df0c3646394b92fc075442fe6f01b17fd
austinxduong <austinxduong@gmail.com>
2026-09-08 12:13:43 -0700

    hasDuplicate optimized

--- 02-arrays/contains-duplicate.js
+++ 02-arrays/contains-duplicate.js
@@ -17,3 +17,39 @@ function hasDuplicate(nums) {
     // i stays parked, and j does a sweep through the nums array comparing it to the parked i if it matches - return true 
     // O(n²) time - because for every item, where are doing something with every other item, and its a nested loop
     // O(1) space - because where only holding i and j in memory once. regardless of input size
+
+// to refactor to a better time and space complexity 
+// at first this was my answer
+// O(n) - time - because were only looping throught the array once, and we loop through each element proportionate to the size of nums
+// O(n) - space because the space size of mySet grows - taking up the space proportionate to the size of nums 
+function  hasDuplicate(nums) {
+        const mySet = new Set()
+
+        for (let i =0; i < nums.length; i++) {
+            if(mySet.has(nums[i]) === true) {
+                return true
+            } else {
+                mySet.add(nums[i])
+            }
+        }
+        if (mySet.size === nums.length) {
+            return false 
+        }
+    }
+
+
+// then i refactored again and realized this also works with less code
+// O(n) - time - because the set loops through each element, proportionate to the size of nums
+// O(n) - space because the space size of mySet grows - taking up the space proportionate to the size of nums 
+
+function  hasDuplicate(nums) {
+        const mySet = new Set(nums)
+
+        if (mySet.size < nums.length) {
+            return true
+        }
+        return false
+    }
+
+// the trade off between these two is the first if statement checks mySet.has() method checks is mySet has nums[i], and exits and returns true early if met without going through the whole array
+// passing in the iterator in my Set for the second algo, has to check every element regardless - so in the best case the first algo is a better trade off.
