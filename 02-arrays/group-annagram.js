65b8187232ee05af4e9fb959faa63a102d74b4e5
austinxduong <austinxduong@gmail.com>
2026-09-10 16:23:54 -0700

    revised

--- 02-arrays/group-annagram.js
+++ 02-arrays/group-annagram.js
@@ -59,5 +59,5 @@ function groupAnagrams(strs) {
     then return just the values of the hashmap as an array
 */
 
-// O(N •M)- time because we are looping over the array which is proportionate to the size of the array. then inside the loop, we have to invidually sort each letter
+// O(N · M log M) - time because we are looping over the array which is proportionate to the size of the array. then inside the loop, we have to invidually sort each letter
 // O(N •M) - space because first we store key to the size of proportionate to the array length, the total character data stored in hashMap scales with both how many strings there (key) are and how long they are (value)
